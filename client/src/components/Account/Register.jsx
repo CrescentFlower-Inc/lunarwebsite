a45b29b0074dc8c3f4b27b3f428b9c54ff789a57
BadPythonCoder <xsare435@gmail.com>
2023-10-01 13:11:24 +0200

    It's aliiveeegit add .!

--- client/src/components/Account/Register.jsx
+++ client/src/components/Account/Register.jsx
@@ -1,12 +1,15 @@
 import styles from '../Shared/Forms.module.css';
 import TextInput from '../Shared/TextInput';
-import { splitProps } from "solid-js";
+import { splitProps, createSignal } from "solid-js";
 
 
 function Register () {
+	let [form, setForm] = createSignal({"username": "", "email": "", "password": "", "confirm": ""})
+
 	const handleSubmit = (e) => {
-		alert("DUCK!!")
+		console.log(form());
 	}
+
 
 
 	return <>
@@ -14,13 +17,13 @@ function Register () {
 
 		<iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
 		<form class={styles.Form} target="dummyframe">
-			<TextInput label="Username" placeholder="Enter in your username"/>
+			<TextInput label="Username" placeholder="Enter in your username" signal={form} setSignal={setForm} name="username" />
 
-			<TextInput label="Email" placeholder="Enter in your email here"/>
+			<TextInput label="Email" placeholder="Enter in your email here" signal={form} setSignal={setForm} name="email" />
 
-			<TextInput label="Password" placeholder="Enter in your password"/>
+			<TextInput label="Password" placeholder="Enter in your password" signal={form} setSignal={setForm} name="password" />
 
-			<TextInput label="Confirm Password" placeholder="Retype your password"/>
+			<TextInput label="Confirm Password" placeholder="Retype your password" signal={form} setSignal={setForm} name="confirm" />
 
 			<button onclick={handleSubmit}>Register!</button>
 		</form>
--- client/src/components/Shared/TextInput.jsx
+++ client/src/components/Shared/TextInput.jsx
@@ -2,12 +2,20 @@ import { splitProps } from 'solid-js';
 import styles from './Forms.module.css';
 
 function TextInput(props) {
-	const [, inputProps] = splitProps(props, ["label"])
+	const [, inputProps] = splitProps(props, ["label", "signal", "setSignal", "name"])
+
+	const onInput = (e) => {
+		let val = e.currentTarget.value;
+		let copy = props.signal();
+
+		copy[props.name] = val;
+		props.setSignal(copy)
+	}
 
 	return (
 		<div class={styles.TextInput}>
 			<label>{props.label}</label>
-			<input {...inputProps}/>
+			<input {...inputProps} onInput={onInput}/>
 			<p/>
 		</div>
 	)
