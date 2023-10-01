ae557d6bff8215a7aff55e8d7d7eb65e513de9fb
BadPythonCoder <xsare435@gmail.com>
2023-10-01 17:39:38 +0200

    Made server 'properly' serve files

--- server/lunarwebsite/__init__.py
+++ server/lunarwebsite/__init__.py
@@ -1,10 +1,12 @@
 from lunarwebsite.accounts import app as accountsapp
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from pathlib import Path
 
-staticfolder = Path(__file__).parents[2] / "client/dist"
-index = staticfolder / "index.html"
+distfolder = Path(__file__).parents[2] / "client/dist"
+assetsfolder = distfolder / "assets"
+index = distfolder / "index.html"
 
 app = FastAPI()
 
@@ -14,4 +16,14 @@ app.mount("/api/account", accountsapp)
 async def root():
 	return {"Location": "The moon", "Server": "UDN DuckServ 6.9", "Organization": "Crescent-Inc"}
 
-app.mount("/", StaticFiles(directory=str(staticfolder), html=True), name="static")
+app.mount("/assets/", StaticFiles(directory=str(assetsfolder)), name="static")
+
+@app.get("/{duck}", response_class=HTMLResponse)
+async def almost_true_root(duck):
+	with open(index) as f:
+		return f.read()
+
+@app.get("/", response_class=HTMLResponse)
+async def true_root():
+	with open(index) as f:
+		return f.read()
